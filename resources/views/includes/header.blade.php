78080093b32572026b0c5836b32f0378676189d2
Alberto-Bruno <brunoalberto97@outlook.it>
2023-02-28 17:51:07 +0100

    first commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,26 @@
+<div class="info-header">
+    <div class="container">
+        <h6>DC POWER VISA</h6>
+        <h6>ADDITIONAL DC SITES</h6>
+    </div>
+</div>
+<header class="container">
+    <figure class="logo">
+        <a href="{{ url('/') }}"><img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo DC"></a>
+    </figure>
+    <nav>
+        <ul>
+            <li><a href="{{ route('characters') }}">CHARACTERS</a></li>
+            <li><a href="{{ route('comics') }}">COMICS</a></li>
+            <li><a href="{{ route('movies') }}">MOVIES</a></li>
+            <li><a href="{{ route('tv') }}">TV</a></li>
+            <li><a href="{{ route('games') }}">GAMES</a></li>
+            <li><a href="{{ route('collectiobles') }}">COLLECTIOBLES</a></li>
+            <li><a href="{{ route('videos') }}">VIDEOS</a></li>
+            <li><a href="{{ route('fans') }}">FANS</a></li>
+            <li><a href="{{ route('news') }}">NEWS</a></li>
+            <li><a href="{{ route('shop') }}">SHOP</a></li>
+        </ul>
+    </nav>
+</header>
+<div class="jumbo"></div>
